ffa139ecd44086ed3254f95e98bc3509a2bd1c24
Black-Fairy786 <hbdeveloper.21@gmail.com>
2023-07-10 15:53:13 +0500

    change profile pic

--- resources/views/layouts/users/edit-profile.blade.php
+++ resources/views/layouts/users/edit-profile.blade.php
@@ -28,7 +28,7 @@
                                     <div class="card-body">
                                         <div class="text-center chat-image mb-5">
                                             <div class="avatar avatar-xxl chat-profile mb-3 brround">
-                                                <a class="" href="#"><img alt="avatar" src="{{asset('assets/images/users/23.jpeg')}}" class="rounded"></a>
+                                                <a class="" href="#"><img alt="avatar" src="{{asset('assets/images/users/23.jpeg')}}" class="avatar avatar-xxl chat-profile mb-3 brround"></a>
                                             </div>
                                             <div class="main-chat-msg-name">
                                                 <a href="{{url('profile')}}">
