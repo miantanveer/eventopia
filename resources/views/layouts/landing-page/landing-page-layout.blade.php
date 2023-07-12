e847d10239e316d4f742f17362138dfd6eaa291c
Black-Fairy786 <hbdeveloper.21@gmail.com>
2023-07-12 19:44:09 +0500

    working on landing page responsiveness

--- resources/views/layouts/landing-page/landing-page-layout.blade.php
+++ resources/views/layouts/landing-page/landing-page-layout.blade.php
@@ -17,21 +17,26 @@
     @include('layouts.components.styles')
     <style>
         .content {
+            position: relative;
             height: 100vh;
+            background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+            url('{{ asset("assets/images/brand/landing_page_img.jpg") }}');
             background-size: cover;
             background-position: center;
+            background-repeat: no-repeat;
         }
 
         .jumps-prevent {
             padding-top: 0px !important;
         }
 
-        .logo-horizontal {
+        /* .logo-horizontal {
             z-index: 9999 !important;
-        }
+        } */
 
         .header {
             background-color: transparent !important;
+            border-bottom: 0px !important;
         }
 
         .form-control {
@@ -53,11 +58,10 @@
 
         .inline-form {
             position: absolute;
-            top: 38%;
+            top: 50%;
             left: 50%;
             transform: translate(-50%, -50%);
             text-align: center;
-            width: 76%;
         }
 
         .input {
@@ -65,7 +69,76 @@
         }
 
         .border-end {
-            border-color: #ACACAC !important;
+            border-left: #ACACAC !important;
+        }
+
+        .header-right-icons {
+            z-index: 9999 !important;
+        }
+
+        .button {
+            padding: 0.400rem 0.760rem !important;
+        }
+
+        .my-font-size {
+            font-size: 0.79rem !important;
+        }
+
+        @media screen and (max-width: 767px) {
+            .my-font-size {
+                font-size: 0.68rem !important;
+            }
+
+            .font-size {
+                font-size: 0.68rem !important;
+            }
+        }
+
+        @media screen and (max-width: 575px) and (min-width: 344px) {
+            .my-font-size {
+                font-size: 0.9rem !important;
+            }
+
+            .font-size {
+                font-size: 0.9rem !important;
+            }
+
+            .border-end {
+                border: none !important;
+            }
+
+            .border-end0 {
+                border-bottom: #ACACAC solid 1px !important;
+                padding-bottom: 1rem !important;
+            }
+
+            .btn-search {
+                width: 100% !important;
+            }
+
+            .font-size-h1 {
+                font-size: 1.5rem !important;
+            }
+        }
+
+        @media screen and (max-width: 345px) and (min-width: 0px) {
+            .btn-search {
+                width: 100% !important;
+            }
+
+            .border-end {
+                border: none !important;
+            }
+
+            .border-end0 {
+                border-bottom: #ACACAC solid 1px !important;
+                padding-bottom: 1rem !important;
+            }
+
+            .font-size-h1 {
+                margin-bottom: 30px !important;
+                font-size: 1.1rem !important;
+            }
         }
     </style>
 
@@ -81,34 +154,39 @@
     <!-- page -->
     <div class="page">
         <div class="page-main">
-            @include('layouts.components.landing-page-header')
-            <img src="{{ asset('assets/images/brand/landing_page_img.jpg') }}" alt="">
-            <div class="inline-form">
-                <h1 class="h1 text-white fw-bold">Make an event that suits you</h1>
-                <form action="#">
-                    <div class="row bg-whiter justify-content-between text-start py-3 px-5 box-sizing" id="form-border">
+            <div class="content">
+                @include('layouts.components.landing-page-header')
+                <div class="inline-form container my-7 my-sm-3">
+                    <h1 class="h1 text-white font-size-h1 fw-bold">Make an event that suits you</h1>
+                    <form action="#" class="ms-5">
+                        <div class="row bg-whiter justify-content-between text-start py-3 px-5 mx-5 mx-sm-0 box-sizing rounded-3"
+                            id="form-border">
 
-                        <div class="col-4 px-0">
-                            <label class="mb-0 label-none"><b>What have you got planned?</b></label>
-                            <input type="text" class="form-control input py-0 px-0" id="searchTerm1"
-                                placeholder="Enter Your Activity">
+                            <div class="col-sm-4 px-0 border-end border-end0">
+                                <label class="mb-0 label-none my-font-size"><b>What have you got planned?</b></label>
+                                <input type="text" class="form-control input py-0 px-0" id="searchTerm1"
+                                    placeholder="Enter Your Activity">
+                            </div>
+                            <div class="col-sm-4 border-end border-end0 px-0 px-sm-3 my-3 my-sm-0">
+                                <label class="mb-0 label-none font-size"><b>Where?</b></label>
+                                <input type="text" class="form-control input py-0 px-0" id="searchTerm2"
+                                    placeholder="Enter a City or Address">
+                            </div>
+                            <div class="col-sm-3 border-end0 pe-0 px-0 px-sm-3 mb-3 mb-sm-0">
+                                <label class="mb-0 label-none font-size"><b>When?</b></label>
+                                <input type="text" class="form-control input py-0 px-0" id="searchTerm3"
+                                    placeholder="Anytime">
+                            </div>
+                            <div class="col-sm-1 text-end px-0">
+                                <button type="submit"
+                                    class="btn btn-primary rounded text-end button d-none d-sm-block"><i
+                                        class="fa fa-search"></i></button>
+                                <button type="submit"
+                                    class="btn btn-primary btn-search mx-0 px-0 text-center d-sm-none d-block">Search</button>
+                            </div>
                         </div>
-                        <div class="col-4 px-0">
-                            <label class="mb-0 label-none"><b>Where?</b></label>
-                            <input type="text" class="form-control input py-0 px-0" id="searchTerm2"
-                                placeholder="Enter a City or Address">
-                        </div>
-                        <div class="col-3 px-0">
-                            <label class="mb-0 label-none"><b>When?</b></label>
-                            <input type="text" class="form-control input py-0 px-0" id="searchTerm3"
-                                placeholder="Anytime">
-                        </div>
-                        <div class="col-1">
-                            <button type="submit" class="btn btn-primary rounded text-end"><i
-                                    class="fa fa-search"></i></button>
-                        </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
