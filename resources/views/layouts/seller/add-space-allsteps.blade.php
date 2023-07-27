0e3fb6b011e08133f9008c187a60e214894175bb
hbdeveloper-77 <hbdeveloper77@gmail.com>
2023-07-26 23:15:45 -0700

    working on step 2

--- resources/views/layouts/seller/add-space-allsteps.blade.php
+++ resources/views/layouts/seller/add-space-allsteps.blade.php
@@ -4,6 +4,11 @@
     .seller-web-sidebar {
         z-index: 9999;
     }
+
+    #step-2 {
+        width: 900px;
+        margin: 0 auto;
+    }
 </style>
 @endsection
 
@@ -14,15 +19,15 @@
             <div class="card-body">
                 <div id="smartwizard-3">
                     <ul>
-                        <li><a href="#step-10">Address Space</a></li>
-                        <li><a href="#step-11">Setup</a></li>
-                        <li><a href="#step-12">About your Space</a></li>
+                        <li><a href="#step-1">Address Space</a></li>
+                        <li><a href="#step-2">Setup</a></li>
+                        <li><a href="#step-3">About your Space</a></li>
                     </ul>
                     <div>
-                        <h2 style="text-align:center;">Step 1 of 9</h2>
-                        <div id="step-10" class="border">
-                            <form>
 
+                        <div id="step-1" class="border">
+                            <form>
+                                <h2 style="text-align:center;">Step 1 of 9</h2>
                                 <div class="row">
                                     <div class="card-header mb-3">
                                         <h3 class="card-title">Space Address</h3>
@@ -49,51 +54,91 @@
                         </div>
 
 
-                        <div id="step-11" class="border">
+                        <div id="step-2">
                             <div style="text-align:center;">
-                                 <h2 >Step 2 of 9</h2>
-                                    <h2>What type of space are you listing?</h2>
-                                    <p>Enter the type of space that most closely represents the physical space being listed. Learn more</p></div>
-                                   <p style="margin-left:25rem;"> <img src="{{ asset('assets/images/users/spaces/6700.png') }}" alt="img">Examples: 'Apartment' 'Photo Studio' 'Restaurant'</p>
-                                   <input style="width:980px;margin-left:25rem;" type="text" class="form-control rounded-0" id="inputtext" placeholder="Gallery for art">
-                            
+                                <h2>Step 2 of 9</h2>
+                                <h2>What type of space are you listing?</h2>
+                                <p>Enter the type of space that most closely represents the physical space being listed.<span style="color:#006CE4">Learn more</span> </p>
+                            </div>
+                            <p> <img src="{{ asset('assets/images/users/spaces/6700.png') }}" alt="img">Examples: 'Apartment' 'Photo Studio' 'Restaurant'</p>
+                            <input type="text" class="form-control rounded-0" id="inputtext" placeholder="Gallery for art">
+                            <div class="inner-steps mt-5 mb-3">
+                                <h1><strong>Describe the parking options</strong></h1>
+                                <div class="form-check form-switch">
+                                    <p><b>Are there parking options at or near your space?</b>
+                                        <input style="margin-left:25rem;height:22px;width:35px;background-color:#000000" class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked>
+                                        <label class="form-check-label" for="flexSwitchCheckChecked"></label>
+                                    </p>
+                                    <h1><strong>Select all</strong></h1>
+                                </div>
+
+
+
+                            </div>
+
                             <form>
-                                <div class="form-group">
-                                    <label>Street Address *</label>
-                                    <input type="text" class="form-control" id="inputtext" placeholder="Enter User Name">
-                                </div>
-                                <div class="form-group">
-                                    <label>City *</label>
-                                    <input type="email" class="form-control" id="exampleInputEmail8" placeholder="">
-                                </div>
-                                <div class="form-group">
-                                    <label>State</label>
-                                    <input type="password" class="form-control" id="exampleInputPassword9" placeholder="Password">
-                                </div>
-                                <div class="form-group">
-                                    <label>Zip Code *</label>
-                                    <input type="password" class="form-control" id="exampleInputPassword9" placeholder="">
-                                </div>
                                 <div class="form-group mb-0 justify-content-end">
-                                    <div class="">
+                                    <div class="checkbox-inline">
                                         <label class="custom-control custom-checkbox">
                                             <input type="checkbox" class="custom-control-input" name="example-checkbox2" value="option2">
-                                            <span class="custom-control-label">Check me Out</span>
+                                            <span class="custom-control-label">Free onsite parking
+                                            </span>
                                         </label>
                                     </div>
-                                </div>
+
+
+                                    <div class="">
+                                        <label class="custom-control custom-checkbox checkbox-inline">
+                                            <input type="checkbox" class="custom-control-input" name="example-checkbox2" value="option2">
+                                            <span class="custom-control-label">Paid onsite parking</span>
+                                        </label>
+                                    </div>
+
+
+                                    <div class="">
+                                        <label class="custom-control custom-checkbox checkbox-inline">
+                                            <input type="checkbox" class="custom-control-input" name="example-checkbox2" value="option2">
+                                            <span class="custom-control-label">Free street parking</span>
+                                        </label>
+                                    </div>
+
+
+                                    <div class="col-md-5 col-lg-6 mg-t-20 mg-md-t-0">
+                                        <label class="custom-control custom-checkbox">
+                                            <input type="checkbox" class="custom-control-input" name="example-checkbox2" value="option2">
+                                            <span class="custom-control-label">Valet</span>
+                                        </label>
+                                    </div>
+
+
+                                    <div class="col-md-5 col-lg-6">
+                                        <label class="custom-control custom-checkbox">
+                                            <input type="checkbox" class="custom-control-input" name="example-checkbox2" value="option2">
+                                            <span class="custom-control-label">Metered street parking</span>
+                                        </label>
+                                    </div>
+
+
+                                    <div class="col-md-5 col-lg-6 mg-t-20 mg-md-t-0">
+                                        <label class="custom-control custom-checkbox">
+                                            <input type="checkbox" class="custom-control-input" name="example-checkbox2" value="option2">
+                                            <span class="custom-control-label">Nearby parking lot</span>
+                                        </label>
+                                    </div>
+
                             </form>
                         </div>
                     </div>
-                    <div id="step-12" class="">
-                        <div class="form-group mb-0 justify-content-end">
-                            <div class="">
-                                <label class="custom-control custom-checkbox">
-                                    <input type="checkbox" class="custom-control-input" name="example-checkbox2" value="option2">
-                                    <span class="custom-control-label">I agree terms &
-                                        Conditions</span>
-                                </label>
-                            </div>
+                </div>
+
+                <div id="step-3" class="">
+                    <div class="form-group mb-0 justify-content-end">
+                        <div class="col-md-5 col-lg-6">
+                            <label class="custom-control custom-checkbox">
+                                <input type="checkbox" class="custom-control-input" name="example-checkbox2" value="option2">
+                                <span class="custom-control-label">I agree terms &
+                                    Conditions</span>
+                            </label>
                         </div>
                     </div>
                 </div>
@@ -102,6 +147,7 @@
     </div>
 </div>
 </div>
+
 
 @endsection
 
