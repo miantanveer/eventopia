cece3a627fc44938b0c5c8b7faa49c9cecd0bf40
zubairmughal100 <zubairmughal1001@gmail.com>
2023-08-01 19:38:08 +0500

    completed file structure

--- resources/views/content/seller/add-space.blade.php
+++ resources/views/content/seller/add-space.blade.php
@@ -391,11 +391,6 @@
 
     <!--modal-->
 
-
-
-
-
-
     @include('layouts.components.modal')
 
     @yield('modal')
@@ -404,35 +399,9 @@
     <!-- page -->
 
     <!-- global-helper -->
-    <div id="global-helper" class="position-fixed bottom-0 end-0 p-3" style="z-index: 9999;">
-        <div class="dropdown btn-group mt-2 mb-2">
-            <div class="btn-group mt-2 mb-2">
-                <button class="btn btn-pill btn-white border-info text-end dropdown-toggle" type="button"
-                    data-bs-toggle="dropdown" id="global-helper-btn">Do
-                    you need Help?</button>
-                <div class="dropdown-menu w-260" style="">
-                    <form class="card-body pt-3" id="contactForm">
-                        <div class="form-group">
-                            <input class="form-control" type="email" placeholder="Enter Email">
-                        </div>
-                        <div class="form-group">
-                            <input class="form-control" placeholder="Enter your Name" type="text">
-                        </div>
-                        <div class="form-group">
-                            <input class="form-control" placeholder="Enter Phone Number" type="text">
-                        </div>
-                        <div class="submit">
-                            <button class="btn btn-primary btn-block">Submit</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- global-helper closed -->
+    @include()
 
     @include('layouts.components.dark-footer')
-
 
     @include('layouts.components.scripts')
 
