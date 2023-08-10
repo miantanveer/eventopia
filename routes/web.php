e67b45912b2300f0a058199183f9cb3d6f3e224b
hbdeveloper22 <hbdeveloper.22@gmail.com>
2023-08-10 19:52:00 +0500

    middleware on all pages

--- routes/web.php
+++ routes/web.php
@@ -32,117 +32,108 @@ Route::get('reset-password', [AuthenticationController::class, 'resetPasswordInd
 Route::post('reset-password', [AuthenticationController::class, 'resetPassword']);
 Route::post('resend-otp', [AuthenticationController::class, 'sendOtp'])->name('resend-otp');
 
-// Route::get('/forget-password-1', function () {
-//     return view('content.auth.forget-password-1');
-// });
-// Route::get('/reset-password', function () {
-//     return view('content.auth.reset-password');
-// });
-// Route::get('/verify-code-1', function () {
-//     return view('content.auth.verify-code-1');
-// });
-
 //Logout
 Route::post('logout', [AuthenticationController::class, 'logout'])->name('logout');
 
 Route::group(['middleware' => ['auth']], function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    Route::get('/add-space', function () {
+        return view('content.seller.add-space');
+    });
+    Route::get('/edit-profile', function () {
+        return view('content.edit-profile');
+    });
+    Route::get('/notify-list', function () {
+        return view('layouts.components.notify-list');
+    });
+    Route::get('/search-results', function () {
+        return view('content.customer.search-results');
+    });
+    Route::get('/talent-&-entertainments', function () {
+        return view('content.customer.search-results-of-talent&entertainment');
+    });
+    Route::get('/services', function () {
+        return view('content.customer.services');
+    });
+    Route::get('/space-details', function () {
+        return view('content.customer.space-detail');
+    });
+    Route::get('/service-details', function () {
+        return view('content.customer.service-detail');
+    });
+    Route::get('/checkout', function () {
+        return view('layouts.components.checkout-page');
+    });
+    Route::get('/review-pay', function () {
+        return view('layouts.components.review');
+    });
+    Route::get('/payment-successfull', function () {
+        return view('layouts.components.payment-successfull');
+    });
+    Route::get('/bookings', function () {
+        return view('layouts.components.bookings-sample');
+    });
+    Route::get('/booking', function () {
+        return view('layouts.components.booking');
+    });
+    
+    // Seller side
+    // backend k waqt sab ka prefix /seller/ lgana
+    Route::get('/seller-dashboard', function () {
+        return view('content.seller.dashboard');
+    });
+    Route::get('/add-space', function () {
+        return view('content.seller.add-space');
+    });
+    Route::get('/list-space', function () {
+        return view('content.seller.list-space');
+    });
+    Route::get('/space-form-steps', function () {
+        return view('content.seller.space-form-steps');
+    });
+    Route::get('/list-entertainment', function () {
+        return view('content.seller.list-entertainment');
+    });
+    Route::get('/add-entertainment', function () {
+        return view('content.seller.add-entertainment');
+    });
+    Route::get('/entertainment-form-steps', function () {
+        return view('content.seller.entertainment-form-steps');
+    });
+    Route::get('/list-service', function () {
+        return view('content.seller.list-service');
+    });
+    Route::get('/add-service', function () {
+        return view('content.seller.add-services');
+    });
+    Route::get('/service-form-steps', function () {
+        return view('content.seller.service-form-steps');
+    });
+    Route::get('/steps-form-submit', function () {
+        return view('content.seller.steps-form-submit');
+    });
+    Route::get('/my-listings', function () {
+        return view('content.seller.my-listing');
+    });
+    Route::get('/pending-bookings', function () {
+        return view('content.seller.pending-bookings');
+    });
+    Route::get('/create-quote', function () {
+        return view('content.seller.create-quote');
+    });
+    Route::get('/payments', function () {
+        return view('content.seller.payments');
+    });
+    Route::get('/calendar', function () {
+        return view('content.seller.calendar');
+    });
+    Route::get('/operating-hours', function () {
+        return view('content.seller.operating-hours');
+    });
 });
 
 
-Route::get('/add-space', function () {
-    return view('content.seller.add-space');
-});
-Route::get('/edit-profile', function () {
-    return view('content.edit-profile');
-});
-Route::get('/notify-list', function () {
-    return view('layouts.components.notify-list');
-});
-Route::get('/search-results', function () {
-    return view('content.customer.search-results');
-});
-Route::get('/talent-&-entertainments', function () {
-    return view('content.customer.search-results-of-talent&entertainment');
-});
-Route::get('/services', function () {
-    return view('content.customer.services');
-});
-Route::get('/space-details', function () {
-    return view('content.customer.space-detail');
-});
-Route::get('/service-details', function () {
-    return view('content.customer.service-detail');
-});
-Route::get('/checkout', function () {
-    return view('layouts.components.checkout-page');
-});
-Route::get('/review-pay', function () {
-    return view('layouts.components.review');
-});
-Route::get('/payment-successfull', function () {
-    return view('layouts.components.payment-successfull');
-});
-Route::get('/bookings', function () {
-    return view('layouts.components.bookings-sample');
-});
-Route::get('/booking', function () {
-    return view('layouts.components.booking');
-});
-
-// Seller side
-// backend k waqt sab ka prefix /seller/ lgana
-Route::get('/seller-dashboard', function () {
-    return view('content.seller.dashboard');
-});
-Route::get('/add-space', function () {
-    return view('content.seller.add-space');
-});
-Route::get('/list-space', function () {
-    return view('content.seller.list-space');
-});
-Route::get('/space-form-steps', function () {
-    return view('content.seller.space-form-steps');
-});
-Route::get('/list-entertainment', function () {
-    return view('content.seller.list-entertainment');
-});
-Route::get('/add-entertainment', function () {
-    return view('content.seller.add-entertainment');
-});
-Route::get('/entertainment-form-steps', function () {
-    return view('content.seller.entertainment-form-steps');
-});
-Route::get('/list-service', function () {
-    return view('content.seller.list-service');
-});
-Route::get('/add-service', function () {
-    return view('content.seller.add-services');
-});
-Route::get('/service-form-steps', function () {
-    return view('content.seller.service-form-steps');
-});
-Route::get('/steps-form-submit', function () {
-    return view('content.seller.steps-form-submit');
-});
-Route::get('/my-listings', function () {
-    return view('content.seller.my-listing');
-});
-Route::get('/pending-bookings', function () {
-    return view('content.seller.pending-bookings');
-});
-Route::get('/create-quote', function () {
-    return view('content.seller.create-quote');
-});
-Route::get('/payments', function () {
-    return view('content.seller.payments');
-});
-Route::get('/calendar', function () {
-    return view('content.seller.calendar');
-});
-Route::get('/operating-hours', function () {
-    return view('content.seller.operating-hours');
-});
 // Route::get('/seller-profile', function () {
 //     return view('layouts.seller.seller-profile');
 // });
