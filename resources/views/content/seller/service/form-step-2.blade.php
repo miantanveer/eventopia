a801c68bebd948fa65fc2de3651834cca3482549
muhammadzubair7068888 <muhammadzubair7068888@gmail.com>
2023-08-18 20:29:07 +0500

    Updated

--- resources/views/content/seller/service/form-step-2.blade.php
+++ resources/views/content/seller/service/form-step-2.blade.php
@@ -12,10 +12,8 @@
         .sw-btn-group-extra {
             display: none;
         }
-
         .dz-remove {
-            color: red !important;
-            /* Change to your desired color */
+            color: red !important; /* Change to your desired color */
         }
     </style>
 @endsection
@@ -65,7 +63,7 @@
                                     </ul>
                                 </div>
                             </div>
-                            <input type="hidden" name="url" id="url" value='{{ route('service-form-3', $id) }}'>
+                            <input type="hidden" name="url" id="url" value='{{ route('service-form-3',$id) }}'>
 
                             <form action="{{ route('service_form_2') }}" method="post" id="filedrop" class="dropzone">
                                 @csrf
@@ -124,7 +122,6 @@
     <script>
         var myDropzone;
         var alertShown = false; // Flag to track whether the alert has been shown
-        var alertShown8 = false; // Flag to track whether the alert has been shown
 
         Dropzone.autoDiscover = false;
 
@@ -135,10 +132,8 @@
             addRemoveLinks: true,
             acceptedFiles: ".png, .jpeg",
             init: function() {
-                var uploadedFiles = 0; // To keep track of successfully uploaded files
                 this.on("success", function(file, response) {
-                    uploadedFiles++; // Increment the count of uploaded files
-                    if (uploadedFiles) {
+                    if (myDropzone.getQueuedFiles().length === 0 && myDropzone.getUploadingFiles().length === 0) {
                         var id = $('#service_id').val();
                         var url = $('#url').val();
                         window.location.replace(url);
@@ -150,10 +145,6 @@
                         alertShown = true;
                         alert("Please upload at least 4 files.");
                     }
-                    if (!alertShown8 && myDropzone.files.length > 8) {
-                        alertShown8 = true;
-                        alert("Max files allowed are 8.");
-                    }
                 });
             },
         });
@@ -161,11 +152,8 @@
         function upload() {
             if (alertShown && myDropzone.files.length < 4) {
                 alert("Please upload at least 4 files.");
-            } else if (alertShown8 && myDropzone.files.length > 8) {
-                alertShown8 = true;
-                alert("Max files allowed are 8.");
             }
-            else {
+            else{
                 var files = myDropzone.files;
                 myDropzone.processQueue(files);
             }
