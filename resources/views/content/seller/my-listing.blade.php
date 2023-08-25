d045254cbf17d3b559a5252d5587511305f616f4
muhammadzubair7068888 <muhammadzubair7068888@gmail.com>
2023-08-25 12:33:41 +0500

    Updated

--- resources/views/content/seller/my-listing.blade.php
+++ resources/views/content/seller/my-listing.blade.php
@@ -40,7 +40,6 @@
                         </div>
                     </div>
                 </div>
-                {{-- @dd($service[0]->serviceImages,$entertainment[0]->entertainmentActivities) --}}
                 <div class="card-body p-0 mt-5">
                     <div class="col-12 px-0 px-sm-2">
                         <div class="card border">
