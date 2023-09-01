e4b0744b53144ed7e850bc1cdfe70a2b194fcde0
muhammadzubair7068888 <muhammadzubair7068888@gmail.com>
2023-09-01 13:17:48 +0500

    Updated

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Helpers\NotificationEvent;
+use App\Events\NotificationEvent;
 
 class NotificationController extends UserBaseController
 {
