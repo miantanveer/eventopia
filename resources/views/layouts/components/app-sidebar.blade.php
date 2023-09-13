4db56ee68adfd724f73841fa17d90931bd51af75
hybreathe43 <hbdeveloper.43@gmail.com>
2023-09-13 21:15:20 +0500

    updates

--- resources/views/layouts/components/app-sidebar.blade.php
+++ resources/views/layouts/components/app-sidebar.blade.php
@@ -1,12 +1,12 @@
             <!--app-sidebar-->
             <div class="sticky">
                 <div class="app-sidebar__overlay" data-bs-toggle="sidebar"></div>
-                <div class="app-sidebar  test-bar ">
+                <div class="app-sidebar bg-primary">   //test-bar => this class is use to change the color of sidebar(black)
                     <div class="main-sidemenu">
                         <div class="slide-left disabled" id="slide-left"><svg xmlns="http://www.w3.org/2000/svg" fill="#7b8191" width="24" height="24" viewBox="0 0 24 24"><path d="M13.293 6.293 7.586 12l5.707 5.707 1.414-1.414L10.414 12l4.293-4.293z"/></svg></div>
                         <ul class="side-menu">
                             <li class="slide">
-                                <a class="side-menu__item" data-bs-toggle="slide" href="{{URL('/dashboard')}}"><i class="side-menu__icon text-white fe fe-home"></i><span class="side-menu__label ">{{lang('Dashboard')}}</span></a>
+                                <a class="side-menu__item" data-bs-toggle="slide" href="{{URL('/dashboard')}}"><i class="side-menu__icon text-white fe fe-home"></i><span class="side-menu__label text-white">{{lang('Dashboard')}}</span></a>
                             </li>
                             <li class="slide">
                                 <a class="side-menu__item" data-bs-toggle="slide" href="{{route('spaces')}}"><i class="side-menu__icon text-white fa fa-calendar"></i><span class="side-menu__label text-white">{{lang('Book a Space')}}</span></a>
