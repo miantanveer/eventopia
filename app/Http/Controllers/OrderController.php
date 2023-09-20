71f20c9334bb8e90a9b4212d22f80c44325ffbe4
hbdeveloper22 <hbdeveloper.22@gmail.com>
2023-09-20 12:49:45 +0500

    seller dashboard fixing

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Order;
+use App\Models\Quote;
 
 class OrderController extends UserBaseController
 {
@@ -28,7 +29,7 @@ class OrderController extends UserBaseController
             });
         })->where('status', '!=', 0)->take(5)->get();
 
-        $this->upComingBookings = Order::where(function ($query) {
+        $this->activeBookings = Order::where(function ($query) {
             $query->whereHas('service', function ($serviceQuery) {
                 $serviceQuery->whereUserId(user_id());
             })->orWhereHas('space', function ($spaceQuery) {
@@ -36,8 +37,8 @@ class OrderController extends UserBaseController
             })->orWhereHas('entertainment', function ($entertainmentQuery) {
                 $entertainmentQuery->whereUserId(user_id());
             });
-        })->whereStatus(1)->count();
-        
+        })->whereStatus(2)->count();
+
         $this->pendingBookings = Order::where(function ($query) {
             $query->whereHas('service', function ($serviceQuery) {
                 $serviceQuery->whereUserId(user_id());
@@ -46,7 +47,7 @@ class OrderController extends UserBaseController
             })->orWhereHas('entertainment', function ($entertainmentQuery) {
                 $entertainmentQuery->whereUserId(user_id());
             });
-        })->whereStatus(0)->count();
+        })->whereStatus(1)->count();
 
         $this->cancelBookings = Order::where(function ($query) {
             $query->whereHas('service', function ($serviceQuery) {
@@ -78,7 +79,13 @@ class OrderController extends UserBaseController
             });
         })->whereStatus(2)->count();
 
-        $this->UpcomingProgress = $this->totalBookingsCount ? ($this->upComingBookings / $this->totalBookingsCount) * 100 : 0;
+        $this->pendingQuoteRequests = Quote::where(function ($query) {
+            $query->whereHas('service', function ($serviceQuery) {
+                $serviceQuery->whereUserId(user_id());
+            });
+        })->whereIn('status', [0, 1])->count();
+
+        $this->activeBookingProgress = $this->totalBookingsCount ? ($this->activeBookings / $this->totalBookingsCount) * 100 : 0;
         $this->pendingProgress = $this->totalBookingsCount ? ($this->pendingBookings / $this->totalBookingsCount) * 100 : 0;
         $this->CancelProgress = $this->totalBookingsCount ? ($this->cancelBookings / $this->totalBookingsCount) * 100 : 0;
         $this->PreviousProgress = $this->totalBookingsCount ? ($this->previousBookings / $this->totalBookingsCount) * 100 : 0;
