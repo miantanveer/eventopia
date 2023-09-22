bb9c25c132826f4e87097a89a2613ee61e30af98
hbdeveloper22 <hbdeveloper.22@gmail.com>
2023-09-22 20:35:28 +0500

    update

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -96,33 +96,33 @@ class OrderController extends UserBaseController
     }
 
 
-    public function test()
-    {
-        try {
-            $date = Carbon::parse(now())->toDateString();
-            $time = Carbon::parse(now())->format('g A');
-            $SpaceEntorders = Order::whereStatus('2')->where('type', '!=', 'service')->where('date', $date)->where('start_time', $time)->get();
-            $Serivceorders = Order::whereStatus('2')->where('type', 'service')->where('date', $date)->get();
-            foreach ($SpaceEntorders as $SpaceEntorder) {
-                $SpaceEntorder->update(['status' => '5']);
-            }
-            foreach ($Serivceorders as $Serivceorder) {
-                $Serivceorder->update(['status' => '5']);
-            }
+    // public function test()
+    // {
+    //     try {
+    //         $date = Carbon::parse(now())->toDateString();
+    //         $time = Carbon::parse(now())->format('g A');
+    //         $SpaceEntorders = Order::whereStatus('2')->where('type', '!=', 'service')->where('date', $date)->where('start_time', $time)->get();
+    //         $Serivceorders = Order::whereStatus('2')->where('type', 'service')->where('date', $date)->get();
+    //         foreach ($SpaceEntorders as $SpaceEntorder) {
+    //             $SpaceEntorder->update(['status' => '5']);
+    //         }
+    //         foreach ($Serivceorders as $Serivceorder) {
+    //             $Serivceorder->update(['status' => '5']);
+    //         }
 
-            $ActiveSpaceEntorders = Order::whereStatus('5')->where('type', '!=', 'service')->where('date', '>=', $date)->orWhere('end_time', $time)->get();
-            $ActiveSerivceorders = Order::whereStatus('5')->where('type', 'service')->where('date', '>=', $date)->get();
+    //         $ActiveSpaceEntorders = Order::whereStatus('5')->where('type', '!=', 'service')->where('date', '>=', $date)->orWhere('end_time', $time)->get();
+    //         $ActiveSerivceorders = Order::whereStatus('5')->where('type', 'service')->where('date', '>=', $date)->get();
 
-            foreach ($ActiveSpaceEntorders as $ActiveSpaceEntorder) {
-                $ActiveSpaceEntorder->update(['status' => '4']);
-            }
+    //         foreach ($ActiveSpaceEntorders as $ActiveSpaceEntorder) {
+    //             $ActiveSpaceEntorder->update(['status' => '4']);
+    //         }
 
-            foreach ($ActiveSerivceorders as $ActiveSerivceorder) {
-                $ActiveSerivceorder->update(['status' => '4']);
-            }
+    //         foreach ($ActiveSerivceorders as $ActiveSerivceorder) {
+    //             $ActiveSerivceorder->update(['status' => '4']);
+    //         }
 
-        } catch (\Throwable $th) {
-            Log::error('Error in AcceptProposals command in cron job: ' . $th->getMessage());
-        }
-    }
+    //     } catch (\Throwable $th) {
+    //         Log::error('Error in AcceptProposals command in cron job: ' . $th->getMessage());
+    //     }
+    // }
 }
