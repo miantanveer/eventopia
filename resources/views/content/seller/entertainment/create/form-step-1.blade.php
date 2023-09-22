84286f2c70af6f7c25fd240dba255f283e1e6232
Black-Fairy786 <hbdeveloper.21@gmail.com>
2023-09-22 11:01:10 +0500

    Modify Enetratinment Pages

--- resources/views/content/seller/entertainment/create/form-step-1.blade.php
+++ resources/views/content/seller/entertainment/create/form-step-1.blade.php
@@ -50,16 +50,6 @@
             <div class="card">
                 <div class="card-body">
                     <div id="smartwizard-3">
-                        {{-- <ul class="justify-content-around">
-                            <li><a href="#step-1">Step 1</a></li>
-                            <li><a href="#step-2">Step 2</a></li>
-                            <li><a href="#step-3">Step 3</a></li>
-                            <li><a href="#step-4">Step 4</a></li>
-                            <li><a href="#step-5">Step 5</a></li>
-                            <li><a href="#step-6">Step 6</a></li>
-                            <li><a href="#step-7">Step 7</a></li>
-                            <li><a href="#step-8">Step 8</a></li>
-                        </ul> --}}
                         <div>
                             @if ($errors->any())
                                 <div class="alert alert-danger">
