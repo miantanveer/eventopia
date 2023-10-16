2840c1a33ca5d696653a84dcebda3c5fabaaf7c2
hbdeveloper22 <hbdeveloper.22@gmail.com>
2023-10-16 20:20:56 +0500

    bookings tab completed in admin panel

--- app/Http/Controllers/Admin/BookingsController.php
+++ app/Http/Controllers/Admin/BookingsController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Entertainment;
 use App\Models\Order;
+use App\Models\Service;
 use App\Models\Space;
 use Illuminate\Http\Request;
 
@@ -42,21 +44,55 @@ class BookingsController extends AdminBaseController
         } catch (\Throwable $th) {
             return redirect()->back()->with('error', 'Something unexpected happened on server. ' . $th->getMessage());
         }
-
     }
 
-    public function detailIndex()
+    public function detailIndex($id, $type)
     {
+        try {
+            if ($type == 'space') {
+                $this->space = Space::find($id);
+            } elseif ($type == 'entertainment') {
+                $this->entertainment = Entertainment::find($id);
+            } elseif ($type == 'service') {
+                $this->service = Service::find($id);
+            }
 
+            $this->type = $type;
+            return view('content.seller.booking-details', $this->data);
+        } catch (\Throwable $th) {
+            return redirect()->back()->with('error', 'Something unexpected happened on server. ' . $th->getMessage());
+        }
     }
 
-    public function updateStatus()
+    public function updateStatus(Request $req)
     {
+        try {
+            $order = Order::find($req->id);
 
-    }
+            if (!$order) {
+                return response()->json(['message' => 'Booking not found', 'status' => 404]);
+            }
 
-    public function deleteListing()
-    {
+            $data = ['status' => $req->status];
+            if($req->status == "2")
+            {
+                $charge = $this->stripe->charges->create([
+                    'amount' => $order->amount * 100,
+                    'currency' => 'sar',
+                    'customer' => $order->user->customer_id,
+                    'source' => $order->stripe_card_id,
+                    'description' => $order->user->first_name . ' ' . $order->user->last_name . ' paid for ' . $order->type . '.',
+                ]);
+                $data['stripe_txn_resp'] = json_encode($charge);
+            }
 
+            $order->update($data);
+
+            // Assuming you're returning a JSON response, you can customize the response as needed.
+            return response()->json(['message' => 'Status updated successfully', 'status' => 200]);
+        } catch (\Exception $e) {
+            \Log::error($e);
+            return response()->json(['message' => 'Error updating status', 'status' => 500]);
+        }
     }
 }
